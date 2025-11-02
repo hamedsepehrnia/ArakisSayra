15e630c1a28c376c273b067bf8257cec811bdd17
hamed <hamed.sepehrnia85@gmail.com>
2025-11-02 15:34:25 +0330

    optimization(vectorized icons)

--- ArakisSayra/settings.py
+++ ArakisSayra/settings.py
@@ -107,32 +107,32 @@ WSGI_APPLICATION = "ArakisSayra.wsgi.application"
 #========================================
 #sqlite database settings
 #========================================
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # ========================================
 
 # ========================================
 # mysql database settings
 # ========================================
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'arakissa_db',
-#         'USER': 'arakissa_user',
-#         'PASSWORD': 'Mahan123@',
-#         'HOST': 'localhost',   # یا آدرس سرور دیتابیس
-#         'PORT': '3306',
-#         # 'OPTIONS': {
-#         #     'charset': 'utf8mb4',
-#         #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         # },
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'arakissa_db',
+        'USER': 'arakissa_user',
+        'PASSWORD': 'Mahan123@',
+        'HOST': 'localhost',   # یا آدرس سرور دیتابیس
+        'PORT': '3306',
+        # 'OPTIONS': {
+        #     'charset': 'utf8mb4',
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        # },
+    }
+}
 
 
 # Password validation
